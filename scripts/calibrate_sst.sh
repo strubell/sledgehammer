9053e99b16ed37da9cfd3dbbc48d19dbf8a57362
Emma Strubell <strubell@fb.com>
2020-05-01 14:10:47 -0400

    calibrate

--- scripts/calibrate_sst.sh
+++ scripts/calibrate_sst.sh
@@ -10,6 +10,6 @@ bert_model="bert-base-uncased"
 layers="0_3_5_11"
 
 python scripts/run_calibration.py \
--m "$working_dir/$bert_model/$dataset/experiment_$layers/best.th" \
+-m "$working_dir/$bert_model/$dataset/experiment_${layers}_0/best.th" \
 -v "$data_dir/text_cat/$dataset/dev" \
 $other_args
