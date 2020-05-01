6e79429df8123ec71b8574b4ba6dfeb2a7259c05
Emma Strubell <strubell@fb.com>
2020-05-01 13:55:23 -0400

    train sst script

--- scripts/train_sst.sh
+++ scripts/train_sst.sh
@@ -3,8 +3,8 @@
 other_args=$@
 
 #working_dir="/checkpoint/strubell/sledgehammer"
-working_dir="/private/home/strubell/research/slegehammer/models"
-data_dir="/private/home/strubell/research/slegehammer/data_dir"
+working_dir="/private/home/strubell/research/sledgehammer/models"
+data_dir="/private/home/strubell/research/sledgehammer/data_dir"
 dataset="SST-2"
 
 bert_model="bert-base-uncased"
