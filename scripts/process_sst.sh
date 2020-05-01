768eef04f99eff60109882f485f84ec6aa307ad6
Emma Strubell <strubell@fb.com>
2020-05-01 11:46:04 -0400

    sst processing

--- scripts/process_sst.sh
+++ scripts/process_sst.sh
@@ -3,8 +3,11 @@
 ORIG_DATA_DIR="/private/home/strubell/research/data/glue_data/"
 LOCAL_DATA_DIR="data_dir/text_cat"
 DATASET="SST-2"
+LOCAL_DATASET_DIR="$LOCAL_DATA_DIR/$DATASET"
+
+mkdir -p $LOCAL_DATASET_DIR
 
 for split in "dev test"; do
   f="$SST_DIR/$split.tsv"
-  awk '{label=$NF; $NF=""; print label"\t"$0}' $f > "$LOCAL_DATA_DIR/$DATASET/$split"
+  awk '{label=$NF; $NF=""; print label"\t"$0}' $f > "$LOCAL_DATASET_DIR/$split"
 done
