7bcb008f6302b68d0b60825bac41c1409565dcd0
Emma Strubell <strubell@fb.com>
2020-05-05 15:53:27 -0400

    sst preprocessing

--- scripts/process_sst.sh
+++ scripts/process_sst.sh
@@ -9,7 +9,7 @@ LOCAL_DATASET_DIR="$LOCAL_DATA_DIR/$DATASET"
 
 mkdir -p $LOCAL_DATASET_DIR
 
-for split in train dev test; do
+for split in test; do
   f="$ORIG_DATASET_DIR/$split.tsv"
   echo "Processing: $f"
   tail -n +2 $f | awk '{label=$NF; $NF=""; print label"\t"$0}' > "$LOCAL_DATASET_DIR/$split"
