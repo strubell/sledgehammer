b648852fdb7b20c0d95956f40779fb62d0428ba0
Emma Strubell <strubell@fb.com>
2020-05-05 14:58:15 -0400

    eval w/ variable margin

--- scripts/evaluate_sst_margin.sh
+++ scripts/evaluate_sst_margin.sh
@@ -8,17 +8,18 @@ dataset="SST-2"
 
 bert_model="bert-base-uncased"
 layers="0_3_5_11"
-margin="5.0"
+margin="1.0"
 
 #model_dir="$working_dir/$bert_model/$dataset/experiment_${layers}_0"
-experiment="margin-$margin-early-exit-shared"
+#experiment="margin-$margin-early-exit-shared"
+experiment="margin-early-exit-shared"
 model_dir="$working_dir/$experiment/$bert_model/$dataset/experiment_${layers}_0"
 
 # dummy temps
 temperatures="1_1_1_1"
 
 conf_start=$( bc -l <<< "$margin * 50" )
-conf_end=$( bc -l <<< "$margin * 100" )
+conf_end=$( bc -l <<< "$margin * 200" )
 conf_by=$( bc -l <<< "$margin * 5" )
 
 for confidence in $( seq $conf_start $conf_by $conf_end ); do
