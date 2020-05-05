40e583f9ebb20d046baa3f5dd84595679f775914
Emma Strubell <strubell@fb.com>
2020-05-05 12:32:28 -0400

    udpate script

--- scripts/evaluate_sst.sh
+++ scripts/evaluate_sst.sh
@@ -12,7 +12,7 @@ layers="0_3_5_11"
 experiment="baseline"
 model_dir="$working_dir/$experiment/$bert_model/$dataset/experiment_${layers}_0"
 
-temperatures="1.2011244297027588_1.224867582321167_1.24701726436615_1.2987957000732422"
+temperatures="1.2443257570266724_1.3058441877365112_1.2728321552276611_1.3453887701034546"
 
 for confidence in $( seq 55 5 100 ); do
   python scripts/run_evaluation.py  \
